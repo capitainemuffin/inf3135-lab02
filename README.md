b381009954bac5544def21397e4259a07e887f2d
sofianeselaoui <sofianeselaoui@gmail.com>
2019-05-11 12:57:34 -0400

    done function somme from exo 4

--- README.md
+++ README.md
@@ -8,11 +8,22 @@
 utiliser gcc avec l'option -g 
 ```
 
-## Exercice 2 
+## Exercice 3 
 
 #### Question 1 
 
 ```
 Le premier argument correspond au chemin entré pour executer le fichier.
 ```
+
+## Exercice 4
+
+#### Question 1
+
+```
+Le compilateur génère le même code pour les 2 boucles. La vitesse varie en fonction de l'état du système.
+```
+
+#### Question 2
+
 
--- hello.c
+++ hello.c
@@ -1,57 +1,78 @@
-
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
+#include <stdlib.h>
 
 
+// déclaration 
+double racineCarre(int x);
+int boucleAvant(int x);
+int boucleApres(int x);
+void ligneCommande(int taille, char * arguments[]);
+int somme(int taille, char * arguments[]);
 
-void ligneCommande(int tailleTableau, char *tableau){
+// main 
+int main(int argc, char * argv[]){
 
-	printf("%d", tailleTableau);
+	//int chiffre = atoi(argv[1]);
+	//int racine = racineCarre(chiffre);
+	//printf("La racine de %d est %d \n", chiffre, racine);
+	//boucleAvant(chiffre);
+	//boucleApres(chiffre);
+	//ligneCommande(argc ,argv);
 
-	
+	printf("Résultat : %d", somme(argc, argv)); 
+}
+
+
+//implementation
+double racineCarre(int x){
+
+
+	return (int) sqrt(x);
 
 }
 
-double racineCarreEntiere(int x){
+int boucleAvant(int x){
 
-	return sqrt(x);
-
-}
-
-
-void boucleAvant(int x){
-
-	for (int i = 0; i < x; ++i){
+	for (int i = 0; i < x; i++) {
 
 		printf("%d\n", i);
 	}
-
-}
-
-void boucleApres(int x){
-
-	for (int i = 0; i < x ; i++) {
-
-		printf("%d\n", i);
-
-	}
-
-
-}
-
-
-
-int main (int argc, char * argv[]){
-
-
-	int x = atoi(argv[1]);
-	//boucleAvant(x);
-	//boucleApres(x);
-
-	//printf("%d", (int)racineCarreEntiere(x));
-
-	ligneCommande(argc, argv);
 
 	return 0;
+
+}
+
+int boucleApres(int x){
+
+	for (int i = 0 ; i < x ; ++i){
+
+		printf("%d\n", i);
+
+	}
+	return 0;
+}
+
+
+void ligneCommande(int taille, char * arguments[]){
+
+	for (int i = 0; i < taille; i++){
+
+		printf("Argument #%d : %s\n", i, arguments[i]);
+	}
+}
+
+int somme(int taille, char * arguments []){
+
+	int somme = 0; 
+
+	for (int i = 0; i < taille; i++){
+
+		int chiffre = atoi(arguments[i]);
+		somme += chiffre;
+		
+	}
+
+	return somme;
+
 }
