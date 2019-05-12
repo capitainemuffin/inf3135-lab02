6994d000adad92603144d057cd99e7d2703252fa
sofianeselaoui <sofianeselaoui@gmail.com>
2019-05-11 22:26:57 -0400

    fini exo 4

--- hello.c
+++ hello.c
@@ -9,18 +9,21 @@ int boucleAvant(int x);
 int boucleApres(int x);
 void ligneCommande(int taille, char * arguments[]);
 int somme(int taille, char * arguments[]);
-
+int estTrie(int taille, char * arguments []);
+int elementFrequent(int taille, char * arguments[]);
 // main 
 int main(int argc, char * argv[]){
 
-	//int chiffre = atoi(argv[1]);
-	//int racine = racineCarre(chiffre);
-	//printf("La racine de %d est %d \n", chiffre, racine);
+	int chiffre = atoi(argv[1]);
+	int racine = racineCarre(chiffre);
+	printf("La racine de %d est %d \n", chiffre, racine);
 	//boucleAvant(chiffre);
 	//boucleApres(chiffre);
 	//ligneCommande(argc ,argv);
 
-	printf("Résultat : %d", somme(argc, argv)); 
+	//printf("Résultat : %d", somme(argc, argv));
+	//printf("%d\n", estTrie(argc, argv)); 
+	//printf("Valeur récurrente : %d\n", elementFrequent(argc, argv)); 
 }
 
 
@@ -74,5 +77,50 @@ int somme(int taille, char * arguments []){
 	}
 
 	return somme;
+
+}
+
+int estTrie(int taille, char * tableau[]){
+
+	for (int i = taille-1 ; i > 0 ; i--) {
+
+		if (atoi(tableau[i]) < atoi(tableau[i-1])){
+
+			return 1; 
+		}
+	}	
+
+	return 0;
+
+} 
+
+int elementFrequent(int taille, char * tableau[]){
+
+	int valeur_recurrente = atoi(tableau[1]);
+	int occurence = 1;
+
+	for (int i = 1 ; i < taille-1 ; i++ ){
+	
+		int tmp_occurence = 1;
+
+		for(int j = i+1 ; j < taille ; j++) {
+								
+			if(atoi(tableau[i]) == atoi(tableau[j])) {
+					
+				tmp_occurence++;					
+					
+			}
+			
+		}
+
+		if (tmp_occurence > occurence){
+
+			valeur_recurrente = atoi(tableau[i]);
+			occurence = tmp_occurence;
+ 
+		}
+	}
+
+	return valeur_recurrente;
 
 }
